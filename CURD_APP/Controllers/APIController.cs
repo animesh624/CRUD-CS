f71b6104d9ed28ed8afd5be220a600e4795ba998
Animesh Dubey <Animesh.Dubey@KornFerry.com>
2023-06-24 10:52:21 +0530

    Added API for app

---
+++ CURD_APP/Controllers/APIController.cs
@@ -0,0 +1,110 @@
+﻿using CURD_APP.Data;
+using CURD_APP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+//namespace keyword is used to declare a scope that contains a set of related objects. 
+namespace CURD_APP.Controllers
+{
+    [Route("api/v1")]
+    //[ApiController]  // this tells that this class is an API controller
+    public class APIController : ControllerBase  // manage operations related to controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public APIController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public ActionResult<List<Model1>> getItems()
+        {
+            List<Model1> items = _db.Dish.ToList();
+            return Ok(items);
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<Model1> getItem(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Model1 item = _db.Dish.FirstOrDefault(u => u.Id == id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteItem(int? id)
+        {
+            if(id==null)
+            {
+                return BadRequest();
+            }
+            Model1 item = _db.Dish.FirstOrDefault(u => u.Id == id);
+
+            if (item==null)
+            {
+                return NotFound();
+            }
+            _db.Dish.Remove(item);
+            _db.SaveChanges();
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateItem([FromBody]Model1 obj)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (obj.Id>0)
+            {
+                return BadRequest();
+            }
+            //List<Model1> list = _db.Dish.ToList();
+            //if (list.Count == 0)
+            //{
+            //    obj.Id = 1;
+            //}
+            //else
+            //{
+            //    int largestId = list.OrderByDescending(item => item.Id).Select(item => item.Id).FirstOrDefault();
+            //    obj.Id = largestId + 1; 
+            //}
+            _db.Dish.Add(obj);
+            _db.SaveChanges();
+
+            return Ok(obj);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateItem(int? id,[FromBody] Model1 obj)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id==null)
+            {
+                return BadRequest();
+            }
+      
+            Model1 item=_db.Dish.FirstOrDefault(u=>u.Id == id);
+            if(item==null)
+            {
+                return NotFound();
+            }
+
+            item=
+
+            return Ok(obj);
+        }
+
+    }
+}
---
+++ CURD_APP/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CURD_APP.Models;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+namespace CURD_APP.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Model1> Dish { get; set; }
+
+
+    }
+}
